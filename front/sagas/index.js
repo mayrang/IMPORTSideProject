25e80824c4051550e3f3f2c3e3de19f0530f0942
mayrang <pkss0626@naver.com>
2022-08-05 13:06:14 +0900

    백엔드 포트 대응

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -3,7 +3,7 @@ import {fork, all} from "redux-saga/effects"
 import postSaga from "./post";
 import userSaga from "./user";
 
-axios.defaults.baseURL = "http://localhost:3065";
+axios.defaults.baseURL = "http://localhost:3080";
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
