f320a9327125076047146ce721a4afc1b0b73ad9
tadaHP <gusvy0220@gmail.com>
2022-08-06 15:26:18 +0900

    change response body - login, signup, me

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import 'express-async-errors';
 import * as userRepository from '../data/auth.js';
 import { config } from '../config.js';
+import * as reservationRepository from '../data/reservation.js'
 
 // POST/auth/signup
 export async function signup(req, res){
@@ -12,9 +13,7 @@ export async function signup(req, res){
     const foundEmail = await userRepository.findByEmail(email);
     
     // let errorMessage = {studentId : `${studentId}`,phoneNumber : `${phoneNumber}`,email : `${email}`};
-
     // let errorMessage = {};
-
     // if(foundStudentId){
     //     // errorMessage+= `"studentId":"${studentId}"`;
     //     errorMessage["studentId"] = [];
@@ -33,9 +32,6 @@ export async function signup(req, res){
     //     errorMessage["email"].push(`${email}`);
     // }
     // JSON.stringify(errorMessage);
-
-
-
     // if(foundStudentId || foundPhoneNumber || foundEmail){
     //     return res.status(409).json(errorMessage);//오류코드
     // }
@@ -56,14 +52,25 @@ export async function signup(req, res){
         email
     });
     const token = createJWTToken(memberId);
-    res.status(201).json({ token, studentId });
+
+    const returnValue  = 
+    {
+        "jwtToken" : `${token}`,
+        "myInfo" : {
+            "id": `${memberId}`,
+            "name": `${name}`,
+            "Posts" : []
+        }
+    };
+    JSON.stringify(returnValue);
+
+    res.status(201).json(returnValue);
 }
 
 // POST/auth/login
 export async function login(req, res) {
     const { studentId, password } = req.body;
     const member = await userRepository.findByStudentId(studentId);
-    console.log(member);
     if(!member){
         console.log("iderr");
         return res.status(401).json({ message: 'Invalid member or password'});
@@ -74,18 +81,45 @@ export async function login(req, res) {
         return res.status(401).json({ message: 'Invalid member or password '});
     }
     const token = createJWTToken(member.id);
-    console.log(token);
-    res.status(200).json({ token, studentId });
+
+    console.log(await reservationRepository.getByMemberId(member.id));
+
+    const returnValue  = 
+    {
+        "jwtToken" : `${token}`,
+        "myInfo" : {
+            "id": `${member.id}`,
+            "name": `${member.name}`,
+            "Posts" : await reservationRepository.getByMemberId(member.id)
+        }
+    };
+    JSON.stringify(returnValue);
+
+    res.status(200).json(returnValue);
 }
 
 // // GET/auth/me -- check valid member by id
-// export async function me(req, res, next){
-//     const user = await userRepository.findById(req.userId);
-//     if(!user){
-//         return res.status(404).json({ message: 'User not found' });
-//     }
-//     res.status(200).json({ token: req.token, email: user.email });
-// }
+export async function me(req, res, next){
+    const member = await userRepository.findById(req.memberId);
+    if(!member){
+        return res.status(404).json({ message: 'member not found' });
+    }
+
+    console.log(member);
+
+    const returnValue  = 
+    {
+        "jwtToken" : `${req.token}`,
+        "myInfo" : {
+            "id": `${member.id}`,
+            "name": `${member.name}`,
+            "Posts" : await reservationRepository.getByMemberId(member.id)
+        }
+    };
+    JSON.stringify(returnValue);
+
+    res.status(200).json(returnValue);
+}
 
 // // POST/auth/newpassword
 // export async function newpassword(req, res, next){
